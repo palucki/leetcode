aeb96dc89f18839b01312d4ebc22f23352a2ea14
palucki <piotrek.palucki@gmail.com>
2024-04-09 17:10:58 +0200

    Completed 151. Reverse words in a string - inplace

--- 151_reverse_words_in_a_string.cpp
+++ 151_reverse_words_in_a_string.cpp
@@ -27,6 +27,46 @@ string reverseWords(string s)
     return result;
 }
 
+// reverses in place, but does not remove whitespaces
+string reverseWordsInPlace(string s)
+{
+    // first swap - words swapped, but reversed 
+    // e.g. hello world -> dlrow olleh
+    for(int i = 0; i < s.length() / 2; ++i)
+    {
+        std::swap(s[i], s[s.length()-i-1]);
+    }
+
+    std::cout << "First swap: " << s << '\n';
+
+    // second swap -> words correctly swapped 
+    // e.g dlrow olleh -> world hello
+    for(int i = 0; i < s.length(); ++i)
+    {
+        if(s[i] == ' ')
+        {
+            continue;
+        }
+
+        // find j -> index of first whitespace
+        int j = i;
+        while(j < s.length() && s[j] != ' ') ++j;
+
+        std::cout << "swapping between " << i << " and " << j << '\n';
+        
+        //swap everything between i and j
+        for(int k = 0; k < (j - i) / 2; ++k)
+        {
+            std::cout << k << " and " << j-k-1 << '\n';
+            std::swap(s[i+k], s[j-1-k]);
+        }
+
+        i = j;
+    }
+
+    return s;
+}
+
 void test(const string& str)
 {
     std::cout << str << " reversed: " << reverseWords(str) << '\n';
