99df9d51831e2e73dc874335e70304d2af8111ae
palucki <piotrek.palucki@gmail.com>
2024-04-17 17:48:28 +0200

    Completed 189. Rotate array using reversing array twice with pivot point

--- 189_rotate_array.cpp
+++ 189_rotate_array.cpp
@@ -1,33 +1,46 @@
 #include <iostream>
 #include <vector>
-#include <unordered_map>
+#include <algorithm>
 
 using namespace std;
+
+// void rotate(vector<int>& nums, int k) 
+// {
+//     k = k % nums.size();
+
+//     const auto get_new_pos = [&nums](const int k, const int old_pos)
+//     {
+//         return (old_pos + k) % nums.size();
+//     };
+
+//     // to start with
+//     int new_index = 0;
+//     int value = nums[(nums.size() - k) % nums.size()];
+
+//     std::cout << "new index = " << new_index << " put value " << value << '\n';
+
+//     for(int n = 0; n < nums.size(); ++n)
+//     {
+//         std::cout << "use val = " << value << " at index " << new_index << " and remember old val = " << nums[new_index] << '\n';
+
+//         int tmp = nums[new_index];
+//         nums[new_index] = value;
+//         value = tmp;
+//         new_index = get_new_pos(k, new_index);
+//     }
+// }
 
 void rotate(vector<int>& nums, int k) 
 {
     k = k % nums.size();
+    //it turns out it is enough to reverse array in a smart way to obtain the rotation result
 
-    const auto get_new_pos = [&nums](const int k, const int old_pos)
-    {
-        return (old_pos + k) % nums.size();
-    };
+    // first reverse whole array
+    std::reverse(nums.begin(), nums.end());
 
-    // to start with
-    int new_index = 0;
-    int value = nums[(nums.size() - k) % nums.size()];
-
-    std::cout << "new index = " << new_index << " put value " << value << '\n';
-
-    for(int n = 0; n < nums.size(); ++n)
-    {
-        std::cout << "use val = " << value << " at index " << new_index << " and remember old val = " << nums[new_index] << '\n';
-
-        int tmp = nums[new_index];
-        nums[new_index] = value;
-        value = tmp;
-        new_index = get_new_pos(k, new_index);
-    }
+    // next reverse separated arrays, from 0 to k-1, from k to end
+    std::reverse(nums.begin(), nums.begin() + k);
+    std::reverse(nums.begin() + k, nums.end());
 }
 
 void print(const std::vector<int>& v)
@@ -51,11 +64,11 @@ void test(std::vector<int> nums, int k)
 
 int main(int argc, char** argv)
 {
-    test({1,2, 3, 4}, 2);
-    // test({1,2,3}, 6);
-    // test({1,2,3,4,5,6,7}, 3);
-    // test({1,2,3,4}, 2);
-    // test({-1,-100,3,99}, 2);
+    test({1, 2, 3, 4}, 2);
+    test({1,2,3}, 6);
+    test({1,2,3,4,5,6,7}, 3);
+    test({1,2,3,4}, 2);
+    test({-1,-100,3,99}, 2);
 
     return 0;
 }
