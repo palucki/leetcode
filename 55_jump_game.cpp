c675d8fdd7c42d8d0ab8305c7e3bbe84631ba77d
palucki <piotrek.palucki@gmail.com>
2024-04-20 16:45:43 +0200

    Completed 55. Jump Game using recursive call (DP)

--- 55_jump_game.cpp
+++ 55_jump_game.cpp
@@ -4,28 +4,53 @@
 
 using namespace std;
 
-bool canJump(vector<int>& nums) 
+bool canJumpRecursively(vector<int>& nums, int i)
 {
-    std::vector<bool> can_reach(nums.size(), false);
+    // std::cout << "at " << i << '\n';
+    if (i < 0 || i > nums.size() - 1) return false;
+    if (i == nums.size() - 1) return true;
+    
+    int val = nums[i];
 
-    can_reach[0] = true;
-    for(int i = 0; i < nums.size(); ++i)
+    for(int j = 1; j <= val; j++)
     {
-        if(!can_reach[i])
+        int jumps_to = i + j;
+        if(canJumpRecursively(nums, jumps_to))
         {
-            break;
-        }
-
-        int num = nums[i];
-        for(int j = 0; j <= num && j+i < nums.size(); ++j)
-        {
-            // std::cout << "from " << i << " can reach " << j+i << '\n';
-            can_reach[j+i] = true;
+            return true;
         }
     }
 
-    return can_reach[can_reach.size() - 1];
+    return false;
 }
+
+bool canJump(vector<int>& nums) 
+{
+    return canJumpRecursively(nums, 0);
+}
+
+// bool canJump(vector<int>& nums) 
+// {
+//     std::vector<bool> can_reach(nums.size(), false);
+
+//     can_reach[0] = true;
+//     for(int i = 0; i < nums.size(); ++i)
+//     {
+//         if(!can_reach[i])
+//         {
+//             break;
+//         }
+
+//         int num = nums[i];
+//         for(int j = 0; j <= num && j+i < nums.size(); ++j)
+//         {
+//             // std::cout << "from " << i << " can reach " << j+i << '\n';
+//             can_reach[j+i] = true;
+//         }
+//     }
+
+//     return can_reach[can_reach.size() - 1];
+// }
 
 void print(const std::vector<int>& v)
 {
