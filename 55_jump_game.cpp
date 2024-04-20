da703cdba1ed29e82af3fd26a632de7d7f51d16d
palucki <piotrek.palucki@gmail.com>
2024-04-20 17:13:02 +0200

    Completed 55. Jump Game using simple approach based on problem

--- 55_jump_game.cpp
+++ 55_jump_game.cpp
@@ -48,7 +48,7 @@ bool canJumpRecursively(vector<int>& nums)
     return canJumpRecursively(nums, 0, memo);
 }
 
-bool canJump(vector<int>& nums) 
+bool canJumpTabulation(vector<int>& nums) 
 {
     std::vector<bool> can_reach(nums.size(), false);
 
@@ -71,6 +71,20 @@ bool canJump(vector<int>& nums)
     return can_reach[can_reach.size() - 1];
 }
 
+bool canJump(vector<int>& nums) 
+{
+    int max_reach = 0;
+
+    for(int i = 0; i < nums.size(); ++i)
+    {
+        if(max_reach < i) return false;
+
+        max_reach = std::max(max_reach, i + nums[i]);
+    }
+
+    return true;
+}
+
 void print(const std::vector<int>& v)
 {
     for(const auto& e : v)
@@ -88,8 +102,8 @@ void test(std::vector<int> nums)
 
 int main(int argc, char** argv)
 {
-    // test({2,3,1,1,4});
-    // test({3,2,1,0,4});
+    test({2,3,1,1,4});
+    test({3,2,1,0,4});
     std::chrono::time_point<std::chrono::steady_clock> start = std::chrono::steady_clock::now();
 
     test({2,0,6,9,8,4,5,0,8,9,1,2,9,6,8,8,0,6,3,1,2,2,1,2,6,5,3,1,2,2,6,4,2,4,3,0,0,0,3,8,2,4,0,1,2,0,1,4,6,5,8,0,7,9,3,4,6,6,5,8,9,3,4,3,7,0,4,9,0,9,8,4,3,0,7,7,1,9,1,9,4,9,0,1,9,5,7,7,1,5,8,2,8,2,6,8,2,2,7,5,1,7,9,6});
