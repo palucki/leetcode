3be074bcbc4db204f44f983e6e498601c70135d5
palucki <piotrek.palucki@gmail.com>
2024-05-15 19:16:25 +0200

    Completed 3. Longest substring without repeating characters with a single pass algorithm (keep track of character and position)

--- 3_longest_substring_without_repeating_chars.cpp
+++ 3_longest_substring_without_repeating_chars.cpp
@@ -7,6 +7,36 @@ using namespace std;
 
 int lengthOfLongestSubstring(string s) 
 {
+    if(s.empty())
+    {
+        return 0;
+    }
+    
+    std::unordered_map<char, int> already_in_substr;
+    already_in_substr.insert({s[0], 1});
+    int longest = 1;
+    int l = 0;
+    
+    for(int i = 1; i < s.size(); ++i)
+    {
+        if(already_in_substr.find(s[i]) != already_in_substr.end())
+        {
+            // already in the substr, need to move "window" to the elem right to it
+            l = std::max(already_in_substr[s[i]], l);
+        }
+
+        already_in_substr[s[i]] = i+1;
+
+        longest = std::max(longest, i - l + 1);
+        // std::cout << "new longest =  " << longest << " i = " << i << " l = " << l << '\n'; 
+    }
+
+    return longest;
+}
+
+
+int lengthOfLongestSubstringSet(string s) 
+{
     std::size_t longest = 0;
     std::unordered_set<char> already_in_substr;
     for(int i = 0; i < s.size(); ++i)
@@ -46,7 +76,7 @@ int main(int argc, char** argv)
     test("abcabcbb");
     test("bbbbb");
     test("pwwkew");
-    test(" ");
+    test("abba");
 
     return 0;
 }
